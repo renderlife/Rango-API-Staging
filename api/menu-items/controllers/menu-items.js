d03b3dad85a4ae3c36554c5415697d7f88f3e55f
szerwinski <hugoszervinsk@gmail.com>
2021-03-22 14:30:36 -0300

    new route /match in menu-items model

--- api/menu-items/controllers/menu-items.js
+++ api/menu-items/controllers/menu-items.js
@@ -1,8 +1,16 @@
-'use strict';
+const { sanitizeEntity } = require('strapi-utils');
 
-/**
- * Read the documentation (https://strapi.io/documentation/v3.x/concepts/controllers.html#core-controllers)
- * to customize this controller
- */
+module.exports = {
+  
 
-module.exports = {};
+  async findMatch(ctx) {
+    let entities;
+    if (ctx.query._q) {
+      entities = await strapi.services["menu-items"].search(ctx.query);
+    } else {
+      entities = await strapi.services["menu-items"].find(ctx.query, ['restaurant']);
+    }
+
+    return entities.map(entity => sanitizeEntity(entity, { model: strapi.models["menu-items"] }));
+  },
+};
