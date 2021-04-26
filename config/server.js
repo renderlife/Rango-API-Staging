a481b24183671e66cd5de448962d3384ae83e3a4
szerwinski <hugoszervinsk@gmail.com>
2021-04-26 18:51:18 -0300

    added domain url name in server config

--- config/server.js
+++ config/server.js
@@ -1,6 +1,7 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
+  url: 'https://www.staging.rangosemfila.com.br',
   admin: {
     auth: {
       secret: env('ADMIN_JWT_SECRET'),
