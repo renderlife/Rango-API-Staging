ede40a481ba57b27b69e1eb79bfe2a07b682a173
szerwinski <hugoszervinsk@gmail.com>
2021-05-07 16:16:01 -0300

    added localhost permission

--- config/middleware.js
+++ config/middleware.js
@@ -3,7 +3,7 @@ module.exports = ({ env }) => ({
         cors: {
           enabled: true,
           credentials: true,
-          origin: ['http://staging.rangosemfila.com.br', 'https://staging.rangosemfila.com.br', 'http://www.staging.rangosemfila.com.br', 'https://www.staging.rangosemfila.com.br'] // Change to My Dashboard domain later
+          origin: ['http://staging.rangosemfila.com.br', 'https://staging.rangosemfila.com.br', 'http://www.staging.rangosemfila.com.br', 'https://www.staging.rangosemfila.com.br', 'http://localhost:8080', 'http://172.20.10.7:8080', 'http://localhost:8080/', 'http://172.20.10.7:8080/'] // Change to My Dashboard domain later
         },
     },
 });
